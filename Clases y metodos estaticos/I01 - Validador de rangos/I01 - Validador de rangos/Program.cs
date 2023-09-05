7ee4ec833adc66f0d28334ea77ed4c55b02cd083
Your Name <you@example.com>
2023-09-04 22:44:19 -0300

    first commit

--- Clases y metodos estaticos/I01 - Validador de rangos/I01 - Validador de rangos/Program.cs
+++ Clases y metodos estaticos/I01 - Validador de rangos/I01 - Validador de rangos/Program.cs
@@ -8,7 +8,7 @@ namespace I01___Validador_de_rangos
         /* Realizar una clase llamada Validador que posea un método estático llamado Validar con la siguiente firma:
          * bool Validar(int valor, int min, int max)
          * valor: dato a validar.
-         * min: mínimo valor incluido.,
+         * min: mínimo valor incluido.
          * max: máximo valor incluido.
          * Pedir al usuario que ingrese 10 números enteros. 
          * Validar con el método desarrollado anteriormente que estén dentro del rango -100 y 100.
